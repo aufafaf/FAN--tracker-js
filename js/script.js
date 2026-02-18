03c1b1a2fbcaade93a216940b29772381b8b9a93
aufafaf <mfaufalahatx@gmail.com>
2026-02-18 23:05:01 +0700

    css

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const balance = document.getElementById("balance")
+const income = document.getElementById("balance")
+const expense = document.getElementById("balance")
+const transaction = document.getElementById("transaction-from")
+const balance = document.getElementById("balance")
+const balance = document.getElementById("balance")
+
