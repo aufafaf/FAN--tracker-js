2278ddcaa95d6825766b186e15763e93ee3600d0
Aufa <mfaufalahatx@gmail.com>
2026-02-24 23:34:34 +0700

    complete add transaction and add save to local storage

--- js/script.js
+++ js/script.js
@@ -42,21 +42,21 @@ console.log("Amount:", amountInput);
 console.log("Balance:", balance);
 console.log("list transaction:", transactionList);
 
-localStorage.setItem(
-  "transaction",
-  JSON.stringify([
-    { id: 1, name: "Gaji", amount: 5000000, type: "income" },
-    { id: 2, name: "Salary", amount: 100000, type: "income" },
-  ]),
-);
+// localStorage.setItem(
+//   "transaction",
+//   JSON.stringify([
+//     { id: 1, name: "Gaji", amount: 5000000, type: "income" },
+//     { id: 2, name: "Salary", amount: 100000, type: "income" },
+//   ]),
+// );
 
 // localStorage.removeItem(transaction);
 // localStorage.clear();
 init();
 
 console.log(transaction.length);
-console.log(transaction[0].name);
-console.log(transaction[1].name);
+// console.log(transaction[0].name);
+// console.log(transaction[1].name);
 
 function addTransaction(e) {
   e.preventDefault();
@@ -69,6 +69,46 @@ function addTransaction(e) {
   console.log(name);
   console.log(amount);
   console.log(type);
+
+  if (name === "" || name === null) {
+    alert("Nama transaksi tidak boleh kosong");
+    return;
+  }
+
+  if (isNaN(amount) || amount < 0) {
+    alert("Jumlah harus angka positif");
+    return;
+  }
+
+  console.log("Validation passed");
+
+  const transaksi = {
+    id: Date.now(),
+    name: name,
+    amount: amount,
+    type: type,
+  };
+
+  console.log("Transaction: ", transaksi);
+
+  transaction.push(transaksi);
+  console.log("All transaction", transaction);
+
+  transactionForm.reset();
+  nameInput.focus();
+
+  console.log("Form cleared");
+
+  saveTransaction();
+
+  alert("Transaction berhasil ditambahkan");
+}
+
+function saveTransaction() {
+  const jsonString = JSON.stringify(transaction);
+
+  localStorage.setItem("transaction", jsonString);
+  console.log("Saved to localstorage");
 }
 
 transactionForm.addEventListener("submit", addTransaction);
